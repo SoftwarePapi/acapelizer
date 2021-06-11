190a2f6afb1b56f41184de64d043387847713119
muffy <mhrgrj@gmail.com>
2021-06-10 21:01:28 -0400

    added separator instance, API reference, +gitignore samples

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
 # can start by taking in 1 music file and splitting into WAVs
+# API REFERENCE: https://github.com/deezer/spleeter/wiki/4.-API-Reference
+
+from spleeter.separator import Separator
+
+# Using embedded configuration.
+separator = Separator('spleeter:2stems')
+
+
+
